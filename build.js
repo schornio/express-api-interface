0199137ab2cb8434f3e2c57d3e8ce9b0f9377106
Thomas Schorn <thomas@schorn.io>
2020-02-18 19:35:32 +0100

    [patch] include README.md in build

--- build.js
+++ build.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const { copyFile } = fs.promises;
 
 copyFile(`${__dirname}/package.json`, `${__dirname}/build/package.json`);
+copyFile(`${__dirname}/README.md`, `${__dirname}/build/README.md`);
